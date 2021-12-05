a1f5e8b28a285ef8b52dd9241b4b73ce17d94b0a
min0618 <jh3672@gmail.com>
2021-12-05 14:22:36 +0900

    코드정리

--- teddy_project/urls.py
+++ teddy_project/urls.py
@@ -4,21 +4,6 @@ import analysis_app.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', analysis_app.views.index),
-    path('testdg1/', analysis_app.views.dg1),
-    path('dg1data/', analysis_app.views.dg1data),
-    path('testdg2/', analysis_app.views.dg2),
-    path('testdg3/', analysis_app.views.dg3),
-    path('dg3data/', analysis_app.views.dg3data),
-    path('testdg4/', analysis_app.views.dg4),
-    path('dg4data1/', analysis_app.views.dg4data1),
-    path('dg4data2/', analysis_app.views.dg4data2),
-    path('dg4data3/', analysis_app.views.dg4data3),
-    path('dg4data4/', analysis_app.views.dg4data4),
-    path('dg4data5/', analysis_app.views.dg4data5),
-    path('dg4data6/', analysis_app.views.dg4data6),
-    path('dg4data7/', analysis_app.views.dg4data7),
-    path('testdg5/', analysis_app.views.dg5),
     path('testdg6/', analysis_app.views.dg6),
     path('dg6data/', analysis_app.views.dg6data),
 ]
