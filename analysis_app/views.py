5cfbf1745566636df2300fc5ebd2dd543208e78b
min0618 <jh3672@gmail.com>
2021-12-07 12:04:49 +0900

    최종

--- analysis_app/views.py
+++ analysis_app/views.py
@@ -1,26 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from analysis_app.models import Sentiment
-# from django.http import HttpResponse, HttpResponseRedirect
-# import random
-# import pandas as pd
-# import matplotlib.pyplot as plt
-# import seaborn as sns
-import json
-from django.core.serializers.json import DjangoJSONEncoder
-# from datetime import datetime
+from django.http import JsonResponse
+from AI.bring_face_data import *
+import re
 
 def dg6(request) :
     return render(request, 'test6.html', None)
 
 def dg6data(request):
-
     data = []
-    result = [0.079816328, 0.0213168518, 0.1004901064, 0.3011716791, 0.53008148161, 0.00311483803, 0.090287909]
-    senti = ['Anger', 'Disgust', 'Fear', 'Happy',
-             'Sad', 'Surprise', 'Neutral']
+    recordSoundList = RecordSound.objects.all().order_by('-date').first()
+    ai_data = str(ecognizeFeeling(recordSoundList.imgUrl))
+    new_data = re.sub("[^0123456789\.]", " ", ai_data)
+    result = new_data.split()
+    result2 = list(map(float, result))
+
+    senti = ['화남', '싫음', '두려움', '즐거움',
+             '슬픔', '놀람', '중립']
 
     for i in range(len(senti)):
-        data.append({"label": senti[i], "y": result[i]})
+        data.append({"label": senti[i], "y": result2[i]})
 
     return JsonResponse(data, safe=False)
